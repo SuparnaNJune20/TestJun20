13c6bf1d48409d36674e7203b6d73dc7180eb184
SuparnaNJune20 <nikam.suparna.s@gmail.com>
2020-08-08 00:13:12 +0530

    Commit branch1

---
+++ src/p1/CommitConf.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class CommitConf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In branchA file 1");
+		System.out.println("In branchA file 1 Modified ");
+	}
+
+}
